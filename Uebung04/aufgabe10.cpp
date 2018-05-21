1b71933f77f321774204b0cd609c3cc69fec0595
Daphna Beljavskij <damavita2013@gmail.com>
2018-05-21 13:32:52 +0200

    Die Funktionen geben jetzt eine 0 zurueck. Sie als void-Funktionen zu schreiben Funktioniert aus irgendwelchen Gruenden nicht.

--- Uebung04/aufgabe10.cpp
+++ Uebung04/aufgabe10.cpp
@@ -6,32 +6,27 @@ using namespace std;
 
 int main ()
 {
-	int zahl1, zahl2;
-	cout << "1.Zahl: ";
+	int zahl1, zahl2; //Deklarieren der zwei Integer zahl1 und zahl2
+	cout << "1.Zahl: "; //Einlesen von zahl1 und zahl2
 	cin >> zahl1;
 	cout << "2.Zahl: ";
 	cin >> zahl2;
 	
-	cout << "Reference-Swapper: " << endl;
-	int& ref_zahl1 = zahl1;
-	int& ref_zahl2 = zahl2; // c=ref_zahl2, b=ref_zahl1
-	cout << swap_ref(ref_zahl1, ref_zahl2) << endl;
-	//cout << "ref_zahl1: " << ref_zahl1 << " ref_zahl2: " << ref_zahl2 << endl;
-	cout << "Wert von ref_zahl1 danach= " << ref_zahl1 << endl;
-	cout << "Wert von ref_zahl2 danach= " << ref_zahl2 << endl;
+	cout << "Ergebnisse von ref_swap(): " << endl;
+	int& ref_zahl1 = zahl1; //Erstellen von References fuer zahl1 und zahl2
+	int& ref_zahl2 = zahl2; 
+	cout << swap_ref(ref_zahl1, ref_zahl2) << endl; //Aufrufen der Funktion swap_ref() mit Uebergabe von ref_zahl1 und ref_zahl2
+	cout << "Neuer Wert von ref_zahl1: " << ref_zahl1 << endl; //Ausgabe der vertauschten References
+	cout << "Neuer Wert von ref_zahl2: " << ref_zahl2 << endl;
 
-	cout << "Pointer-Swapper: " << endl;
-	cout << swap_ref(ref_zahl2, ref_zahl1) << endl;
-	int* ptr_zahl1 = &ref_zahl1;
+	cout << "Ergebnisse von ref_ptr(): " << endl;
+	/*erneutes Aufrufen von swap_ref() zum "Zuruecktauschen" der Werte von zahl1 und zahl 2, 
+	 * damit swap_ptr() dieselben Anfangswerte hat wie swap_ref(): */
+	cout << swap_ref(ref_zahl2, ref_zahl1) << endl; 
+	int* ptr_zahl1 = &ref_zahl1; //Erstellen von Pointern, die auf die References fuer zahl1 und zahl2 zeigen
 	int* ptr_zahl2 = &ref_zahl2;
-	/*cout << "Adresse ptr_zahl1 vorher: " << ptr_zahl1 << endl;
-	cout << "Wert ptr_zahl1 vorher: " << *ptr_zahl1 << endl;	
-	cout << "Adresse ptr_zahl2 vorher: " << ptr_zahl2 << endl;
-	cout << "Wert ptr_zahl2 vorher: " << *ptr_zahl2 << endl;*/
-
-	cout << swap_ptr(ptr_zahl1, ptr_zahl2) << endl;	
-	//cout << "Adresse ptr_zahl1 danach: " << ptr_zahl1 << endl;
-	cout << "Wert ptr_zahl1 danach: " << *ptr_zahl1 << endl;
-	//cout << "Adresse ptr_zahl2 danach: " << ptr_zahl2 << endl;
-	cout << "Wert ptr_zahl2 danach: " << *ptr_zahl2 << endl;
+	
+	cout <<  swap_ptr(ptr_zahl1, ptr_zahl2) << endl; //Aufrufen von swap_ptr()
+	cout << "Neuer Wert von ptr_zahl1: " << *ptr_zahl1 << endl; //Ausgabe der Werte auf die die Pointer nach swap_ptr() zeigen
+	cout << "Neuer Wert von ptr_zahl2: " << *ptr_zahl2 << endl;
 }
--- Uebung04/swap.cpp
+++ Uebung04/swap.cpp
@@ -5,42 +5,18 @@ using namespace std;
 
 int swap_ref (int& ref_zahl1, int& ref_zahl2)
 {
-	/*cout << "Wert von ref_zahl1 = " << ref_zahl1 << endl;
-	cout << "Wert von ref_zahl2 = " << ref_zahl2 << endl;*/
 	int temp = ref_zahl1;
-	/*cout << "Wert von temp = " << temp << endl;	
-	cout << "Adresse von temp: " << &temp << endl;*/
 	ref_zahl1 = ref_zahl2;
-	//cout << "Wert von ref_zahl1 danach= " << ref_zahl1 << endl;
-	//cout << "Adresse von ref_zahl1 danach: " << &ref_zahl1 << endl;
 	ref_zahl2 = temp;
-	//cout << "Wert von ref_zahl2 danach= " << ref_zahl2 << endl;
-	//cout << "Adresse von ref_zahl2 danach: " << &ref_zahl2 << endl;
-	return ref_zahl1;
+	//return ref_zahl1;
+	return 0;
 }
 
 int swap_ptr (int* ptr_zahl1, int* ptr_zahl2)
 {
 
 	int temp = *ptr_zahl1;
-	//cout << "Wert von temp: " << temp << endl;
-	//cout << "Adresse von temp: " << &temp << endl;
 	*ptr_zahl1 = *ptr_zahl2; //über das * kann man den Pointer derefenzieren und auf den eigentlichen Wert auf den der Pointer zeigt zugreifen
 	*ptr_zahl2 = temp;
-	return *ptr_zahl1;
+	return 0;
 }
-	//int a,x;
-	/*cout << "a = " << a << endl;
-	cout << "x = " << x << endl;*/
-	/*int& b=a;
-	int& c=x;*/
-	/*cout << "b = " << b << endl;
-	cout << "Adresse von b: " << &b << endl;
-	cout << "c = " << c << endl;
-	cout << "Adresse von c: " << &c << endl;*/
-	/*ptr_1 = &b;
-	ptr_2 = &c;*/
-	/*cout << "Wert von ptr_1 = " << (*ptr_1) << endl;
-	cout << "ptr_1 = " << ptr_1 << endl;
-	cout << "Wert von ptr_2 = " << (*ptr_2) << endl;
-	cout << "ptr_2 = " << ptr_2 << endl;*/
