51057300590565fa365467e1d15612c877ddd33a
Maximilian Felix Lethaus <max@lethaus.net>
2018-05-29 15:00:33 +0200

    aufgabe12 added

---
+++ Uebung05/bitmuster.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef BITMUSTER_HPP
+#define BITMUSTER_HPP 1
+
+
+//Methodenkoepf der Bitmuster Funktion
+extern string bitmuster (char zeichen);
+
+
+#endif
