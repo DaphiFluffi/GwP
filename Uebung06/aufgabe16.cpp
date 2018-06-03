7d19e8c77ebb38a0e91372a37a09a4ec90067bab
Daphna Beljavskij <damavita2013@gmail.com>
2018-06-03 14:53:28 +0200

    Die Makefile funktioniert und ich habe aufgabe16 kommentiert.

--- Uebung06/aufgabe16.cpp
+++ Uebung06/aufgabe16.cpp
@@ -6,39 +6,37 @@ using namespace std;
 
 int main (int argc, char** argv)
 {
-	/*string filename; //erstellen eines String filename, damit man den dateinamen einlesen kann 
-	cout << "Datei: "; //einlesen des strings filename vom benutzer
-	cin >> filename;*/
-	
-	cout << "Datei: "; //einlesen des strings filename vom benutzer
+	cout << "Datei: "; // Einlesen des Dateinamens vom User ueber argv[0], weil dort der Dateiname standarsmassig gespeichert wird
 	cin >> argv[0];
 	
-	ifstream input;
-//	input.open (filename, ios::in); 
-	input.open (argv[0], ios::in);
-	if(!input) {  // Überprüfung ob Datei geöffnet wurde //wie macht man das mit argc?
-		//cerr << "Programm beim Oeffnen der Datei " << filename << " gescheitert." << "\n" 
-		//<< "Bitte speichern Sie die zuoeffnende Datei in demselben Verzeichnis wie dieses Programm ab und geben sie beim naechsten Ausfuehren den richtigen Dateinamen ein." << endl;
+	ifstream datei; //Erstellen eines ifstream-Objekts namens datei. Ich benutze ifstream, weil datei nur eingelesen werden soll. 
+	datei.open (argv[0], ios::in); // Oeffnen der Datei mit Uebergabe des eingelesenen Dateinamens argv[0] und des Modus ios::in, um anzugeben, dass die Datei nur num Einlesen verwendet werden soll.
+	if(!datei) {  // Falls man die Datei nicht oeffnen kann ...
+		//... wird mit cerr (dem Standardausgabestream) eine Fehlermeldung ausgegeben ...
 		cerr << "Programm beim Oeffnen der Datei " << argv[0] << " gescheitert." << "\n" 
 		<< "Bitte speichern Sie die zuoeffnende Datei in demselben Verzeichnis wie dieses Programm ab und geben sie beim naechsten Ausfuehren den richtigen Dateinamen ein." << endl;
-		return 1;
+		return 1; //... und das Programm beendet. Mit return 1 wird signalisiert, dass beim Ausfuehren ein Fehler unterlaufen ist.
 	}
-	int i = 0;
-	string line;
-	while (getline(input, line))
+	
+	int i = 0; // Zaehlervariable fuer die while-Schleife
+	string zeile; //string- Variable, die in der getline()-Funktion verwendet wird, um die Datei Zeilenweise auszulesen
+	while (getline(datei, zeile)) //Ausfuehren der getline()-Funktion 
 	{
-		cout << "line: " << line << '\n';
-		double wert;
-		wert = atof(line.c_str()); 
-		cout << "wert: " << wert << endl;
-		double* array = new double[i];
-		array[i] = wert;
-		cout << "array[" << i <<"]: " << array[i] << endl;
-		cout << "i:" << i << endl;
-		i++;
+		//cout << "line: " << line << '\n';
+		double wert; //double-Variable wert, in der die als strings in der Variable zeile gespeicherten ausgelesenen Zahlenwerte, als doubles gespeichert werden sollen.
+		wert = atof(zeile.c_str()); // atof ist ein Befehl mit dem man Strings in doubles umwandeln kann. Als Argument nimmt atof nur const char*. "zeile" ist jedoch eine string-Variable, sodass man diese erstmal durch c_str() umwandeln muss.
+		//cout << "wert: " << wert << endl;
+		double* array = new double[i]; //Erstellen eines double* arrays mit dem new[]-Operator fuer dynamische Speicherverwaltung.
+		array[i] = wert; // Befuellen des arrays an der i-ten Stelle mit den Werten aus der Datei
+		//cout << "array[" << i <<"]: " << array[i] << endl;
+		cout <<  array[i] << endl;
+		//cout << "i:" << i << endl;
+		i++; 
 		//delete[] array;
 	}
-	input.close();
+	datei.close(); //Schliessen der Datei
+	//delete[] array;
+	//cout << "array nach delete: " << array << endl;
 	return 0;
 }
 
