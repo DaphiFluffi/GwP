ffdd4b3f89992f1048c9ff6752234dc83c50e98c
Daphna Beljavskij <damavita2013@gmail.com>
2018-06-19 23:37:31 +0200

    Kommentare von aufgabe 19 und zufallsgenerator

--- Uebung07/Zufallsgenerator.cpp
+++ Uebung07/Zufallsgenerator.cpp
@@ -6,7 +6,12 @@ using namespace std;
 
 typedef long long unsigned int bigint;
 
-//Definition des Konstruktors
+/*Definition des Konstruktors
+ *randGen nimmt beim Erstellen alle Variablen optional entgegen:
+ * x: Startwert, der Zufallskette
+ * a, b, c: Integer, um die x in der Xorshift-Methode verschoben wird
+ * mlcg_a, mlcg_c, mlcg_m: long long unsigned int Variablen fuer die MLCG-Methode
+ * Werden die Variablen nicht angegeben, werden sie mit den empfohlenden Standartwerten besetzt. */
 randGen::randGen(bigint x_, int a_, int b_, int c_, bigint mlcg_a_, bigint mlcg_c_, bigint mlcg_m_)
 {
 	x = x_; //setzt die Klassenattribute mit den Konstruktorparametern gleich
@@ -18,6 +23,7 @@ randGen::randGen(bigint x_, int a_, int b_, int c_, bigint mlcg_a_, bigint mlcg_
 	mlcg_m = mlcg_m_;
 }
 
+// ersetzt das aktuelle X mit einem durch Binaerverschiebung neu erstelltem X
 bigint randGen::xorshift(){
 	x ^= x << a;
 	x ^= x >> b;
@@ -25,17 +31,23 @@ bigint randGen::xorshift(){
 	return x;
 }
 
+// ersetzt das aktuelle X mit einem durch lineare Kongruenz neu erstelltem X
 bigint randGen::mlcg(){
 	x = ((mlcg_a*x + mlcg_c) % mlcg_m);
 	return x;
 }
 
+// wendet Xorshift und MLCG hintereinender an: MLCG(Xorshift)
 bigint randGen::mlcgXorshift(){
 	xorshift();
 	mlcg();
 	return x;
 }
 
+/* castIn nimmt zwei Integers entgegen und gibt ein Double zurueck,
+ * das an der selben Position im angegebenen Intervall steht wie X in [0, 2^64],
+ * dabei wird X (aus randGen) selbst nicht veraendert!
+*/
 double randGen::castIn(int a, int b){
 	return  (double(x/2) / pow(2,63)  *  (b-a)) + a; // rechne mit  2 * 2^63 um das nutzen von 2^64 zu vermeiden so kommt es zu keinen speicherproblemen.
 }
--- Uebung07/aufgabe19.cpp
+++ Uebung07/aufgabe19.cpp
@@ -6,60 +6,61 @@ using namespace std;
 
 typedef long long unsigned int bigint;
 
+// Gauss Funktion gauss(x)=y alternativ koennen noch SIGMA und MY mit angegeben werden
 double gauss(double x, const double SIGMA = 1, const double MY = 0)
 {	
 	return 1/( SIGMA * sqrt(2 * M_PI) ) * exp( -(x - MY)*(x -MY) / (2 * SIGMA * SIGMA) );
 }
 
+// histogramm nimmt ein Array und dessen Laenge entgegen und erstellt daraus ein Histogramm mit n-vielen Intervallen
+// x entspricht den Intervallen und y der Anzahl der Punkte in diesem Intervall
 double* histogramm (double* array, int len, int n)
 {
 	// max und min ermitteln
 	double max = array[0];
 	double min = array[0];
-	for(int i = 0; i < len; i++){
+	for(int i = 0; i < len; i++){ // Zaehlt das Array durch, und ermittelt dabei das Maximum und Minimum
 		if( array[i] < min ){min = array[i];}
 		if( array[i] > max){max = array[i];}
 	}
-	//cout << min << "\t" << max << endl;
-	double intervall = (max - min) / n;
+	double intervall = (max - min) / n; //bestimmt die Groesse eines einzelnen Intervalls 
 	
-	double* histogramm = new double[n];
-	double bereich;
-	for(int balken = 0; balken < n; balken++){
-		bereich =  intervall * balken + min;
-		for(int i = 0; i < len; i++){	
+	double* histogramm = new double[n]; // erstellt das Rueckgabe-Array
+	double bereich; //Punkt an dem das momentane Intervall steht
+	for(int balken = 0; balken < n; balken++){ //zaehlt die einzelnen Intervalle / Balken hoch und fuellt sie
+		bereich =  intervall * balken + min; // passt den Zahlenbereich auf das momentane Intervall an
+		for(int i = 0; i < len; i++){	// geht das auszuwertende Array durch und zahlt die im Intervall liegenden Punkte
 			if( bereich <= array[i] && array[i] < (bereich + intervall) ) {histogramm[balken]++;}
 		}
 	}
-	
 	return histogramm;
 }
 
 int main()
 {
-	const int LEN = 100000;
+	//Setup
+	const int LEN = 100000; //Anzahl der Gauss Werte
 	double* array = new double[LEN];
-	
+	//Initialisiert 2 Zufallszahlketten
 	randGen xRand(92305972093508);
 	randGen yRand(598769760);
-	double x;
+	double x; // Speicherstellen für castIn
 	double y;
-	int i = 0;
 	
+	int i = 0; // Whileschleife, die nach LEN-vielen Werten sucht
 	while ( i < LEN ){
-		xRand.mlcgXorshift();
+		xRand.mlcgXorshift(); // "wuerfelt" neu
 		yRand.mlcgXorshift();
-		x = xRand.castIn(-1,1);
+		x = xRand.castIn(-1,1); // Speicher x und y Zufallswerte in den gewuenschen Intervallen ab
 		y = yRand.castIn(0,1);
 
-		if (y+0.25<= gauss(x)){
-			array[i] = x;
+		if (y+0.25<= gauss(x)){ // Ueberprueft ob der zufaellige y-Wert im Bereich der Gausskurve liegt
+			array[i] = x; // speichert den passenden wert ab und zaehlt hoch
 			i++;
 		}	
-		
 	}
 	
-	// In gauss.dat speichern
+	// In gauss.dat speichern 
 	ofstream dateiG;
 	dateiG.open ("gauss.dat", ios::out);
 			
@@ -68,10 +69,11 @@ int main()
 	}
 
 	dateiG.close();
-	cout << "Gauss datei erstellt." << endl;
+	cout << "Gauss-Datei erstellt." << endl;
 	
 	
-	const int N = 15;
+	// Erstellt ein Histogramm aus den gespeicherten Gauss-Werten
+	const int N = 15; 77 Anzahl der Intervalle des Histogramms
 	double* ausgabe = histogramm(array,LEN,N);
 	
 	// In histogramm.dat speichern
@@ -82,6 +84,6 @@ int main()
 		dateiH << ausgabe[i] << endl;	
 	}
 	dateiH.close();
-	cout << "Histogramm datei erstellt." << endl;
+	cout << "Histogramm-Datei erstellt." << endl;
 }
 
