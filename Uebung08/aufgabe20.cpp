fcf693dc21899229486e7e64c3b20fbf9c40ddbc
Daphna Beljavskij <damavita2013@gmail.com>
2018-06-19 15:10:20 +0200

    Uebung08 auf den neuesten Stand und aufggabe19 mit behobenem bigint Problem.

---
+++ Uebung08/aufgabe20.cpp
@@ -0,0 +1,29 @@
+// modify precision
+#include <iostream>     // std::cout, std::ios
+#include <cmath>
+using namespace std;
+
+
+
+int main () {
+	double ergebnis1 = 0 ;
+	double ergebnis2 = 0 ;
+	double n;
+	n = pow(2,32)-1;
+	for(double i = 1; i <= n; i++){
+		ergebnis1 += 1./(i*i);
+	}
+	for ( double i = n; i >= 1; i--){
+		ergebnis2 += 1./(i*i);
+	}
+	cout.unsetf (ios::floatfield); 
+	cout.precision(16);
+	cout << "Summe bis n: ";
+	cout << ergebnis1 << '\n';
+	cout << "Summe bis 1: ";
+	cout << ergebnis2 << '\n';
+ 
+  return 0;
+}
+
+
--- Uebung08/aufgabe21.cpp
+++ Uebung08/aufgabe21.cpp
@@ -3,6 +3,74 @@
 
 using namespace std;
 
+//Wie machen wir die Modularisierung und das includen? Gerade funkioniert es nicht
+//welche dateien sind abzugeben
+//mein sortieralgorithmus funktioniert nicht wk perfekt
+//brauchen wir die ganzen getter setter methoden wenn ir eh mlcg nicht nutzen?
+
+typedef long long unsigned int bigint;
+const bigint M = 18446744073709551615UL;  //(pow(2,64)-1) wuerde die Speicherstelle ueberladen
+
+class randGen
+{
+	private:
+	bigint x; 	// Startwert 
+	int a, b, c; // variablen fuer xorverschiebung 
+	bigint mlcg_a, mlcg_c, mlcg_m; // variablen fuer MLCG
+	
+	public:
+	randGen (bigint , int , int , int , bigint , bigint , bigint );
+	bigint xorshift();
+	bigint mlcg();
+	bigint mlcgXorshift();
+	double castIn(int a = 0, int b = 1);
+	
+	// Noetig ?
+	int getA () {return (a);}; 
+	int getB () {return (b);}; 
+	int getC () {return (c);}; 
+	//bigint getX() {return (x);};
+	void setA (int neua) { a = neua;}
+	void setB (int neub) { b = neub;}
+	void setC (int neuc) { c = neuc;}
+	void setX (bigint neux) { x = neux;};
+	
+}; 
+
+//Definition des Konstruktors
+randGen::randGen(bigint x_= 4101842887655102017, int a_= 21, int b_= 35, int c_ = 4, bigint mlcg_a_ = 2685821657736338717, bigint mlcg_c_ = 0, bigint mlcg_m_ = M)
+{
+	x = x_; //setzt die Klassenattribute mit den Konstruktorparametern gleich
+	a = a_; 
+	b = b_;
+	c = c_;
+	mlcg_a = mlcg_a_;
+	mlcg_c = mlcg_c_;
+	mlcg_m = mlcg_m_;
+}
+
+bigint randGen::xorshift(){
+	x ^= x << a;
+	x ^= x >> b;
+	x ^= x << c;
+	return x;
+}
+
+bigint randGen::mlcg(){
+	x = ((mlcg_a*x + mlcg_c) % mlcg_m);
+	return x;
+}
+
+bigint randGen::mlcgXorshift(){
+	xorshift();
+	mlcg();
+	return x;
+}
+
+double randGen::castIn(int a, int b){
+	return  (double(x/2) / pow(2,63)  *  (b-a)) + a; // rechne mit  2 * 2^63 um das nutzen von 2^64 zu vermeiden so kommt es zu keinen speicherproblemen.
+}
+
 void swap(double *xp, double *yp)
 {
     double temp = *xp;
@@ -20,6 +88,10 @@ void bubbleSort(double array[], double n)
         if (array[j] > array[j+1]){
            swap(&array[j], &array[j+1]);
            swapped = true;
+			for (int i=0; i<10;i++){
+				cout <<  array[i] << endl;
+			}
+			cout << "blubb" << endl;
         }
      }
      // IF no two elements were swapped by inner loop, then break
@@ -29,45 +101,38 @@ void bubbleSort(double array[], double n)
   }
 }
  
-/* Function to print an array */
+// Function to print an array 
 void printArray(double array[], double size)
 {
 	for (int i = 0; i < size; i++)
-	cout << array[i] << " ";
+	cout << array[i] << endl;
 }
 
-double* arrayBefuellen (int size){
-	double* array = new double;
-	for (int i = 0; i < size; i++){
-		array[i] = random();
+double* arrayBefuellen ()
+{
+	int i = 0; 
+	double* array = new double[i];
+	randGen probe(4101842887655102017); 
+	while(i<10){
+		probe.mlcgXorshift();
+		array[i]=probe.castIn(-5,5);
+		cout << array[i]<< "\n";
+		i++;
+		cout << i << endl;
 	}
 	return array;
-	//delete[] array;
 }
-// Driver program to test above functions
+
 int main()
 {
-   // int array[] = {64, 34, 25, 12, 22, 11, 90};
-    double* array = 0;
-    array = arrayBefuellen(100);
-    int n = sizeof(array)/sizeof(array[0]);
+	//double array[] = {-64., 34., 25., 12., 22., 11., 90., 0.5466860};
+	double* array = 0;
+	array = arrayBefuellen();
+    double n = 10;
     bubbleSort(array, n);
     cout << "Sorted array: " << "\n";
     printArray(array, n);
+    delete[] array;
     return 0;
+	
 }
-/*bubbleSort3(Array A)
-  n = A.size
-  do{
-    newn = 1
-    for (i=0; i<n-1; ++i){
-      if (A[i] > A[i+1]){
-        A.swap(i, i+1)
-        newn = i+1
-      } // ende if
-    } // ende for
-    n = newn
-  } while (n > 1)*/
-
-// Optimized implementation of Bubble sort
-//#include <stdio.h>
