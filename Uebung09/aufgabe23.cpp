cc18aadec03cd24d63d25f7cd8323eaf9a049dab
Daphna Beljavskij <damavita2013@gmail.com>
2018-06-26 19:50:26 +0200

    Makefile bisher nur für aufgabe23

--- Uebung09/aufgabe23.cpp
+++ Uebung09/aufgabe23.cpp
@@ -9,19 +9,21 @@ template <class T> class Ableitung
 	T func;
 	double h; // private Variablen
 	public:
-	Ableitung(T _func, double _h = 10e-5) : func(_func), h(_h) { }
+	Ableitung(T _func, double _h = 0.1) : func(_func), h(_h) { } //10e-5
 	// Implementierung
-	T operator() (double x, bool b = 0)
+	double operator() (double x, bool b = false)
 	{
-		if (b == 0){
+		if (b == false){
 			volatile double temp = x+h;
 			h = temp - x;
-			return func(x+h) - func(x)/h;
+			return (func(temp) - func(x))/h;
 		}else{
-			//volatile double temp = x+h;
-			//h = temp - x;
-			//volatile double temp2 = x-h;
-			return func(x+h)-func(x-h)/2*h;
+			volatile double temp = x+h;
+			h = temp - x;
+			volatile double temp2 = x-h;
+			h = temp2 - x; //es liegt hieran 
+			//donothing(temp){};
+			return (func(temp)-func(temp2))/2*h; 
 		}
 	}
 	double getH () { return h;}
@@ -55,7 +57,8 @@ int main()
 	a(2.0);
 	cout << a(2.0) << endl;//Test Klasse funktioniert es kommt 4 raus 
 	Ableitung <Test> beispiel(a);
-	cout << "Beispiel abgeleitet: " << beispiel(2.0,0) << endl; //funktioniert warum auch immer nicht
+	cout << "Beispiel abgeleitet: " << beispiel(2.0,false) << endl; //funktioniert warum auch immer nicht
+	cout << "Beispiel abgeleitet: " << beispiel(2.0,true) << endl; //funktioniert warum auch immer nicht
 	//beispiel.waehlen();
 	cout << "Blubb" << endl;
 }
